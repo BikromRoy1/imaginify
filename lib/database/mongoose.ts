c413d9bb157ed2eca7d5a030e0b322961bac6095
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-05-04 11:47:29 +0600

    add few files

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,27 +7,26 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+let cached: MongooseConnection = (global as any).mongoose
 
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
+  if(cached.conn) return cached.conn;
 
-    if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-    
-    cached.promise =
-      cached.promise ||
-      mongoose.connect(MONGODB_URL, {
-        dbName: 'imaginify',
-        bufferCommands: false,
-      });
-    cached.conn = await cached.promise;
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-    return cached.conn;
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
